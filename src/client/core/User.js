ebee4fd7aa62ae065acc20b9f98fdf33377674a1
전형탁 <andy395023@gmail.com>
2018-12-11 08:00:47 +0900

    상태, 모델 설계

---
+++ src/client/core/User.js
@@ -0,0 +1,10 @@
+class User {
+    constructor({ id, nickname, avatarUrl }) {
+      this.id = id;
+      this.nickname = nickname;
+      this.avatarUrl = avatarUrl;
+    }
+  }
+  
+  export default User;
+  
